0dda1253bfd7c5ba55e2f7b4fb080461d8b5c405
TaroHaga <pdpark.bb@gmail.com>
2022-06-13 00:09:20 +0900

    途中

--- read.php
+++ read.php
@@ -1,40 +1,16 @@
 
 <?php
+$file = fopen('data/data.txt', 'r'); // ファイルを開く
 
-// ファイルを変数に格納
-$filename = 'date.txt';
- 
-// fopenでファイルを開く（'r'は読み込みモードで開く）
-$fp = fopen($filename, 'r');
-
-while(!feof($fo)){
-
-// fgetsでファイルを読み込み、変数に格納
-$txt = fgets($fp);
- 
-// ファイルを読み込んだ変数を出力
-echo $txt.'<br>';
- 
-}
-
-// fcloseでファイルを閉じる
-fclose($fp);
-
-?>
+// ファイル内容を1行ずつ読み込んで出力
+while ($str = fgets($file)) {
+    echo nl2br($str);// nl2br() ... 改行文字を追加
 
 
-<html>
-<head>
-<meta charset="utf-8">
-<title>READ </title>
-</head>
-<body>
-<h1>tekisuto 
     
-</h1>
-<ul>
-<li><a href="post.php">posix_times.php</a></li>
-</ul>
-</body>
-</html>
+
+
+
+}
+fclose($file); // ファイルを閉じる?>
 
