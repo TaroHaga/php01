db705a340e9a100974126bbf9b115c178ab3604b
TaroHaga <pdpark.bb@gmail.com>
2022-06-12 22:27:01 +0900

    途中

--- post.php
+++ post.php
@@ -4,9 +4,20 @@
 <title>POST練習</title>
 </head>
 <body>
-<form action="write.php" method="post">
-	お名前: <input type="text" name="name">
-	EMAIL: <input type="text" name="mail">
+<form action="write.php" method="post">  
+
+宇多田ヒカルは好きですか？<select class="宇多田">
+<option value="yes">はい😀</option>
+<option value="No">いいえ😞</option>
+</select>
+<br>
+パクチーは好きですか？<select class="paku">
+<option value="yes">はい😀</option>
+<option value="No">いいえ😞</option>
+</select>
+<br>
+	お名前: <input type="text" name="name"> <br>
+	EMAIL: <input type="text" name="mail"><br>
 	<input type="submit" value="送信">
 </form>
 <ul>
